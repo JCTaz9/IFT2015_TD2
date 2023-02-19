af2be9c5900c2178c92b94378630cb618c23bf27
JCTaz9 <82065504+JCTaz9@users.noreply.github.com>
2023-02-18 21:41:35 -0500

    Update Node.java
    Exercise 1

--- src/Node.java
+++ src/Node.java
@@ -3,42 +3,92 @@ public class Node {
     private Node next = null;
 
     public Node(int value){
-        // TODO
+        this.value = value;
     }
 
     public Node(int value, Node next){
-        // TODO
+        this.value = value;
+        this.next = next;
     }
 
     public void addValue(int value){
-        // TODO
+        if (this.next == null) {
+            this.next = new Node(value);
+        } else {
+            this.next.addValue(value);
+        }
     }
 
     public void addNode(Node next){
-        // TODO
+        if (this.next == null) {
+            this.next = next;
+        } else {
+            this.next.addNode(next);
+        }
     }
 
     public void removeLast(){
-        // TODO
+        if (this.next == null) {
+            return;
+        } else if (this.next.next == null) {
+            this.next = null;
+        } else {
+            this.next.removeLast();
+        }
     }
 
     public void removeValue(int value){
-        // TODO
+        if (this.next == null) {
+            return;
+        }
+        while (this.value == value) {
+            this.value = this.next.value;
+            this.next = this.next.next;
+            if (this.next == null) {
+                return;
+            }
+        }
+        this.next.removeValue(value);
     }
 
     public int length_iteratif(){
-        // TODO
-        return 0;
+        int length = 0;
+        Node current = this;
+        while (current != null) {
+            length++;
+            current = current.next;
+        }
+        return length;
     }
 
     public int length_recurssion(){
-        // TODO
-        return 0;
+        if (this.next == null) {
+            return 1;
+        } else {
+            return 1 + this.next.length_recurssion();
+        }
     }
 
     public int returnNlast(int nLast){
-        // TODO
-        return 0;
+
+        if (nLast <= 0) {
+            throw new IllegalArgumentException("nLast doit etre positif");
+        }
+        Node current = this;
+        int length = 0;
+        while (current != null) {
+            current = current.next;
+            length++;
+        }
+        if (nLast > length) {
+            throw new IllegalArgumentException("nLast doit etre moins ou egal a la taille de la liste");
+        }
+        current = this;
+        for (int i = 0; i < length - nLast; i++) {
+            current = current.next;
+        }
+        return current.value;
+
     }
 
     public void addValue_ordered(int value){
